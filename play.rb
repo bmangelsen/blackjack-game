aec26052d538109540d5d9a091cc6531cdb2c2d4
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-19 21:08:00 -0400

    Adjusted method calls to reflect updated names

--- play.rb
+++ play.rb
@@ -6,7 +6,7 @@ game.play = game.greeting
 while game.play == "y"
   game = Blackjack.new(Deck.new)
   game.dealer_display_card
-  game.player_display_cards
+  game.player_display_all_cards
   game.display_player_total
   if game.player_total == 21
     game.player_wins
@@ -18,17 +18,17 @@ while game.play == "y"
     game.move = game.player_move
     while game.move == "hit"
       game.player_draw_card
-      game.player_display_cards
+      game.player_display_all_cards
       if game.player_total == 21
         game.display_player_total
         game.player_wins
         game.play = game.check_if_new_game
-        game.move = "not hit"
+        game.move = ""
       elsif game.player_total > 21
         game.display_player_total
         game.dealer_wins
         game.play = game.check_if_new_game
-        game.move = "not hit"
+        game.move = ""
       else
         game.display_player_total
         game.move = game.player_move
@@ -48,9 +48,9 @@ while game.play == "y"
         end
       end
     end
-    if game.move != "not hit" && game.dealer_total <= 21
+    if game.move != "" && game.dealer_total <= 21
       if game.dealer_total == 21
-        game.player_display_cards
+        game.player_display_all_cards
         game.dealer_display_all_cards
         game.display_dealer_total
         game.display_player_total
@@ -58,21 +58,21 @@ while game.play == "y"
         game.play = game.check_if_new_game
       elsif game.dealer_total == game.player_total
         game.tie
-        game.player_display_cards
+        game.player_display_all_cards
         game.dealer_display_all_cards
         game.display_dealer_total
         game.display_player_total
         game.player_wins
         game.play = game.check_if_new_game
       elsif game.dealer_total > game.player_total
-        game.player_display_cards
+        game.player_display_all_cards
         game.dealer_display_all_cards
         game.display_dealer_total
         game.display_player_total
         game.dealer_wins
         game.play = game.check_if_new_game
       else
-        game.player_display_cards
+        game.player_display_all_cards
         game.dealer_display_all_cards
         game.display_dealer_total
         game.display_player_total
